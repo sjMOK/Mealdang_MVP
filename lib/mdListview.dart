56240b6f2a2c09cc300543c2913dbe6b766a95bf
josunghyuk <mnp1031@naver.com>
2021-05-11 10:08:40 +0900

    add Western Food image&datas.json

--- lib/mdListview.dart
+++ lib/mdListview.dart
@@ -16,49 +16,47 @@ class _MealdangListviewState extends State<MealdangListview> {
   void initState() {
     super.initState();
     datas = [
-    {
-    "cid" : "1",
-    "title": "[프레시지] 우삼겹 순두부찌개",
-    "price": "7900",
-    "review": "7",
-    "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_1.jpeg",
-    "Url" : "https://fresheasy.co.kr/goods/view?no=260"
-    },
       {
-        "cid" : "2",
+        "cid": "1",
+        "title": "[프레시지] 우삼겹 순두부찌개",
+        "price": "7900",
+        "review": "7",
+        "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_1.jpeg",
+        "Url": "https://fresheasy.co.kr/goods/view?no=260"
+      },
+      {
+        "cid": "2",
         "title": "[프레시지] 장흥회관 낙지곱창전골",
         "price": "25900",
         "review": "7",
         "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_2.jpeg",
-        "Url" : "https://fresheasy.co.kr/goods/view?no=635"
+        "Url": "https://fresheasy.co.kr/goods/view?no=635"
 
       },
       {
-        "cid" : "3",
+        "cid": "3",
         "title": "[마이셰프] 인생 닭갈비(2인)",
         "price": "11900",
         "review": "7",
         "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_3.jpeg",
-        "Url" : "https://www.mychef.kr/goods/goods_view.php?goodsNo=1000001231"
-
+        "Url": "https://www.mychef.kr/goods/goods_view.php?goodsNo=1000001231"
       },
       {
         "cid" : "4",
-        "title" : "[삼삼해물] 꽃개&새우 해물탕",
-        "price" : "15800",
-        "review" : "7",
-        "image" : "assets/images/FoodPictures/KoreanFood_list/KoreanFood_4.jpeg",
+        "title": "[삼삼해물] 꽃개&새우 해물탕",
+        "price": "15800",
+        "review": "7",
+        "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_4.jpeg",
         "Url" : "https://shopping.naver.com/fresh/homemeal/stores/100565794/products/4735056755?NaPm=ct%3Dkof823yo%7Cci%3D37a83728379270b3ecc0302ebc3deebe89a1e2a0%7Ctr%3Dslsl%7Csn%3D619584%7Chk%3D588da5f0f9c75e3e26b3bc6584bca65183eadfd9",
       },
       {
-        "cid" : "5",
-        "title" : "[푸드어셈블] 기승전골 곱창전골(2인)",
-        "price" : "25900",
-        "review" : "7",
-        "image" : "assets/images/FoodPictures/KoreanFood_list/KoreanFood_5.jpeg",
-        "Url" : "https://foodasb.co.kr/product/%EB%A7%9B%EC%A7%91%ED%82%A4%ED%8A%B8-%EA%B8%B0%EC%8A%B9%EC%A0%84%EA%B3%A8-%EA%B3%B1%EC%B0%BD%EC%A0%84%EA%B3%A8-%EB%B0%80%ED%82%A4%ED%8A%B82%EC%9D%B8/25/category/49/display/1/",
+        "cid": "5",
+        "title": "[푸드어셈블] 기승전골 곱창전골(2인)",
+        "price": "25900",
+        "review": "7",
+        "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_5.jpeg",
+        "Url": "https://foodasb.co.kr/product/%EB%A7%9B%EC%A7%91%ED%82%A4%ED%8A%B8-%EA%B8%B0%EC%8A%B9%EC%A0%84%EA%B3%A8-%EA%B3%B1%EC%B0%BD%EC%A0%84%EA%B3%A8-%EB%B0%80%ED%82%A4%ED%8A%B82%EC%9D%B8/25/category/49/display/1/",
       },
-
     ];
   }
 
@@ -93,7 +91,6 @@ Widget _myListView(BuildContext context) {
               data: datas[index],
             );
           }));
-
         },
         child: Container(
           color: Colors.transparent, //상품 어디를 눌러도 OnTap가능하게만듬
