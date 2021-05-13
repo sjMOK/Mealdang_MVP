ae481f62d76d270617bb7151172d0b5f9aa5a0a5
josunghyuk <mnp1031@naver.com>
2021-05-13 21:37:02 +0900

    mdCategory change hello to Mealdang

--- lib/food_listview/mdListview.dart
+++ lib/food_listview/mdListview.dart
@@ -35,7 +35,7 @@ class _MealdangListviewState extends State<MealdangListview> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('안녕3',
+        title: Text('밀당',
             style: TextStyle(fontFamily: MyFontFamily.BMJUA, fontSize: 38)),
         backgroundColor: Colors.amber[400],
       ),
