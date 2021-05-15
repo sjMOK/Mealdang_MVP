76eefb98ac874355e81f21506dbfb80aad3f8c09
josunghyuk <mnp1031@naver.com>
2021-05-15 11:34:56 +0900

    Change little UI

--- lib/food_listview/mdListview.dart
+++ lib/food_listview/mdListview.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:mealdang_mvp/detail_page/productInfo.dart';
 import 'package:mealdang_mvp/food_listview/foodData.dart' as foodData;
