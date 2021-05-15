7d1ceba57ce39d9cfca3eb1bfae6a109c7f33f85
josunghyuk <mnp1031@naver.com>
2021-05-15 11:31:35 +0900

    Change Own's page to naver smartstore

--- lib/food_listview/foodData.dart
+++ lib/food_listview/foodData.dart
@@ -7,17 +7,17 @@ List<Map<String, String>> foodData = [
     "review": "7",
     "rating" : "4.6",
     "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_1.jpeg",
-    "Url": "https://fresheasy.co.kr/goods/view?no=260"
+    "Url": "https://shopping.naver.com/fresh/homemeal/stores/100069073/products/3492801815?NaPm=ct%3Dkop4p57k%7Cci%3D0a7e60524ac853fc3d9b0d65e74c5db3c165ad92%7Ctr%3Dslsl%7Csn%3D446884%7Chk%3Da0b07c2bbb5199ddf222f0b9b13b1e1dec98ce7e"
   },
   {
     "cid": "2",
     "title": "[프레시지] 장흥회관 낙지곱창전골",
     "person" : "(2인분)",
-    "price": "25900",
+    "price": "26900",
     "review": "7",
     "rating" : "4.4",
     "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_2.jpeg",
-    "Url": "https://fresheasy.co.kr/goods/view?no=635"
+    "Url": "https://shopping.naver.com/fresh/homemeal/stores/100069073/products/5301558235?NaPm=ct%3Dkop4q2e0%7Cci%3D3f0e2038ced5b70522a266b6cbf8935cb7144718%7Ctr%3Dslsl%7Csn%3D446884%7Chk%3D0c72e350c4aa5a128d112c3e46d15c1c062df302"
   },
   {
     "cid": "3",
@@ -27,7 +27,7 @@ List<Map<String, String>> foodData = [
     "review": "7",
     "rating" : "4.6",
     "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_3.jpeg",
-    "Url": "https://www.mychef.kr/goods/goods_view.php?goodsNo=1000001231"
+    "Url": "https://shopping.naver.com/fresh/homemeal/stores/100073657/products/4858796389?NaPm=ct%3Dkop4nyrs%7Cci%3D3b7ea4f7052a4f76a20c8a2f60a2c32d0250a879%7Ctr%3Dslsl%7Csn%3D458431%7Chk%3D18680b5651286d29ededff914faec71a2aad7598"
   },
   {
     "cid": "4",
@@ -37,8 +37,7 @@ List<Map<String, String>> foodData = [
     "review": "7",
     "rating" : "4.3",
     "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_4.jpeg",
-    "Url":
-        "https://shopping.naver.com/fresh/homemeal/stores/100565794/products/4735056755?NaPm=ct%3Dkof823yo%7Cci%3D37a83728379270b3ecc0302ebc3deebe89a1e2a0%7Ctr%3Dslsl%7Csn%3D619584%7Chk%3D588da5f0f9c75e3e26b3bc6584bca65183eadfd9",
+    "Url": "https://shopping.naver.com/fresh/homemeal/stores/100565794/products/4735056755?NaPm=ct%3Dkof823yo%7Cci%3D37a83728379270b3ecc0302ebc3deebe89a1e2a0%7Ctr%3Dslsl%7Csn%3D619584%7Chk%3D588da5f0f9c75e3e26b3bc6584bca65183eadfd9",
   },
   {
     "cid": "5",
@@ -48,7 +47,6 @@ List<Map<String, String>> foodData = [
     "review": "7",
     "rating" : "4.5",
     "image": "assets/images/FoodPictures/KoreanFood_list/KoreanFood_5.jpeg",
-    "Url":
-        "https://foodasb.co.kr/product/%EB%A7%9B%EC%A7%91%ED%82%A4%ED%8A%B8-%EA%B8%B0%EC%8A%B9%EC%A0%84%EA%B3%A8-%EA%B3%B1%EC%B0%BD%EC%A0%84%EA%B3%A8-%EB%B0%80%ED%82%A4%ED%8A%B82%EC%9D%B8/25/category/49/display/1/",
+    "Url": "https://smartstore.naver.com/foodasb/products/4797604813?NaPm=ct%3Dkop4quxs%7Cci%3D222bf9f8130744f85e0a40f13e2d1b43b8202748%7Ctr%3Dslsl%7Csn%3D549704%7Chk%3Dbb7027185587782c2a57a6d4be1fc3add841ce64",
   },
 ];
