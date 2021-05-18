ce4c7ffd792e8bba87e4e145e70039b325bd9b7d
Hugnry-bird <rnjsgudtjr@naver.com>
2021-05-18 17:58:47 +0900

    Starting Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:mealdang_mvp/category/mdCategory.dart';
 
@@ -7,21 +6,41 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'MealdangMVP',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: AnimatedSplashScreen(
-          splash: "images\Category_icon\CarbSnack_icon\tteokbokki.png",
+    return FutureBuilder(
+        future: Future.delayed(Duration(seconds: 2)),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(home: Splash());
+          } else {
+            return MaterialApp(home: MealdangCategory());
+          }
+        });
+    // return MaterialApp(
+    //     title: 'MealdangMVP',
+    //     theme: ThemeData(
+    //       primarySwatch: Colors.blue,
+    //     ),
+    //     home: AnimatedSplashScreen(
+    //       splash: "images\Category_icon\CarbSnack_icon\tteokbokki.png",
+    //       //"assets\images\Category_icon\CarbSnack_icon\tteokbokki.png",
+    //       nextScreen: MealdangCategory(),
+    //       splashTransition: SplashTransition.fadeTransition,
+    //       backgroundColor: Colors.amber,
+    //       duration: 300,
 
-          //"assets\images\Category_icon\CarbSnack_icon\tteokbokki.png",
-          nextScreen: MealdangCategory(),
-          splashTransition: SplashTransition.fadeTransition,
-          //backgroundColor: Colors.amber,
-          duration: 300,
-        )
-        //MealdangCategory(),
-        );
+    //     )
+    //     //MealdangCategory(),
+    //     );
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Icon(Icons.apartment_outlined,
+          size: MediaQuery.of(context).size.width * 0.785),
+    ));
   }
 }
