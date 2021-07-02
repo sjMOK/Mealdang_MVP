081d33afdd694032e715e6c55850572ab7255256
Hugnry-bird <rnjsgudtjr@naver.com>
2021-07-02 22:21:44 +0900

    applyed survey image

--- lib/page/survey.dart
+++ lib/page/survey.dart
@@ -10,69 +10,62 @@ class Survey extends StatefulWidget {
 class _SurveyState extends State<Survey> {
   @override
   Widget build(BuildContext context) {
-    const Color mainColor = Color.fromRGBO(255, 156, 30, 1);
     return SingleChildScrollView(
-      child: Container(
-        color: mainColor,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Container(color: Colors.red, height: 480.h
-                //child: Image.asset("assets/images/logo/logo_splash.jpg"),
+      child: Column(
+        children: [
+          Container(
+            child: Image.asset("assets/images/survey_image/survey_image.png"),
+          ),
+          SizedBox(height: 5.h),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
                 ),
-            Container(
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.all(Radius.zero)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: mainColor,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Scaffold(
-                            backgroundColor: Colors.white,
-                            body: SafeArea(
-                              child: InAppWebView(
-                                initialUrlRequest: URLRequest(
-                                  url: Uri.parse(
-                                    'https://docs.google.com/forms/d/e/1FAIpQLScwNuDVNimw1J7rCHLzk-bAfzXPzevrTxwyKrvUfJXkUx2POQ/viewform',
-                                  ),
-                                ),
-                              ),
-                            ),
+                side: BorderSide(
+                  width: 1.5.w,
+                  color: Color.fromRGBO(255, 156, 30, 1),
+                )),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Scaffold(
+                    backgroundColor: Colors.white,
+                    body: SafeArea(
+                      child: InAppWebView(
+                        initialUrlRequest: URLRequest(
+                          url: Uri.parse(
+                            'https://docs.google.com/forms/d/e/1FAIpQLScwNuDVNimw1J7rCHLzk-bAfzXPzevrTxwyKrvUfJXkUx2POQ/viewform',
                           ),
                         ),
-                      );
-                    },
-                    child: Container(
-                      height: 42.h,
-                      color: mainColor,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "설문조사하고 스타벅스 받기",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: 'NotoSans',
-                              fontWeight: FontWeight.w900,
-                              fontSize: 18.sp,
-                            ),
-                          ),
-                        ],
                       ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            child: Container(
+              width: 190.w,
+              height: 42.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "설문조사하고 기프티콘 받기",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'NotoSans',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16.sp,
                     ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
