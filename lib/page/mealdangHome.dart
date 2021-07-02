4ecb60aeed7e3015750cccf60f142ded4e1c1e0d
mokseungju <mokseungju@naver.com>
2021-07-02 20:31:20 +0900

    change bottomNavigationBar design

--- lib/page/mealdangHome.dart
+++ lib/page/mealdangHome.dart
@@ -14,6 +14,7 @@ class _MealdangHomeState extends State<MealdangHome> {
   int _selectedIndex;
   Future<Database> database;
   List<Widget> _widgetOptions;
+  DateTime currentBackPressTime;
 
   @override
   void initState() {
@@ -29,10 +30,6 @@ class _MealdangHomeState extends State<MealdangHome> {
     ];
   }
 
-  @override dispose(){
-    print('mdHome dispse');
-    super.dispose();
-  }
 
   void _onItemTapped(int index) {
     if (index == 1) {
@@ -78,45 +75,70 @@ class _MealdangHomeState extends State<MealdangHome> {
           backgroundColor: Colors.white,
         ),
         body: IndexedStack(
-          index: _selectedIndex,
-          children: _widgetOptions,
-        ),
+            index: _selectedIndex,
+            children: _widgetOptions,
+          ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.home,
               ),
-              label: 'Home',
+              label: '홈',
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.search,
               ),
-              label: 'Search',
+              label: '검색',
             ),
             BottomNavigationBarItem(
               icon: Icon(
-                Icons.category,
+                Icons.category_rounded,
               ),
-              label: 'Category',
+              label: '카테고리',
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.email,
               ),
-              label: 'Servey',
+              label: '설문조사',
             ),
           ],
           selectedItemColor: mainColor,
           unselectedItemColor: Colors.grey,
           selectedIconTheme: IconThemeData(color: mainColor),
           unselectedIconTheme: IconThemeData(color: Colors.grey),
+          showUnselectedLabels: true,
+          selectedFontSize: 12.0,
           currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
           onTap: _onItemTapped,
         ),
       ),
-      onWillPop: () {},
+      onWillPop: _onWillPop,
     );
+  }
+
+  Future<bool> _onWillPop() {
+    DateTime now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime) > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: Duration(milliseconds: 1500),
+          content: Text('뒤로가기 버튼을 한번 더 누르면 앱이 종료됩니다'),
+          backgroundColor: const Color.fromRGBO(5, 5, 5, 0.8),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+
+      print('종료');
+      return Future.value(false);
+    }
+    return Future.value(true);
   }
 }
