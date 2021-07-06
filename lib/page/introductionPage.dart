ce56e1e5de815f151287bc3ea6a568df25f6330f
mokseungju <mokseungju@naver.com>
2021-07-06 21:52:05 +0900

    introductionPage no text, only full image

--- lib/page/introductionPage.dart
+++ lib/page/introductionPage.dart
@@ -2,76 +2,96 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
+
 class IntroductionPage extends StatefulWidget {
   @override
   _IntroductionPageState createState() => _IntroductionPageState();
 }
 
 class _IntroductionPageState extends State<IntroductionPage> {
-  void _onIntroEnd(BuildContext context) {
-    print('intro done');
-    Navigator.of(context).pushReplacementNamed('/home');
-  }
-
+  double _width, _height;
   @override
   Widget build(BuildContext context) {
-    double _width = MediaQuery.of(context).size.width;
-    TextStyle bodyStyle = TextStyle(fontSize: _width * 0.05);
-    PageDecoration pageDecoration = PageDecoration(
-      titleTextStyle:
-          TextStyle(fontSize: _width * 0.07, fontWeight: FontWeight.w700),
-      bodyTextStyle: bodyStyle,
-      descriptionPadding:
-          EdgeInsets.fromLTRB(_width * 0.02, 0, _width * 0.02, _width * 0.02),
-      pageColor: Colors.white,
-      imagePadding: EdgeInsets.zero,
-      imageFlex: 3,
-      bodyAlignment: Alignment.bottomCenter,
-    );
-    Widget _buildImage(String assetName, [double width]) {
-      return Image.asset('assets/images/$assetName', width: width);
-    }
+    _width = MediaQuery.of(context).size.width;
+    _height = MediaQuery.of(context).size.height;
 
     return ScreenUtilInit(
       designSize: Size(411.5, 683.4),
       builder: () => Scaffold(
         body: IntroductionScreen(
           globalBackgroundColor: Colors.white,
-          pages: [
-            PageViewModel(
-              title: "밀키트",
-              body: "정리 안된 밀키트 카테고리별로 정리",
-              image: _buildImage(
-                  'introduction_image/IntroductionImage_1.png', 510.w),
-              decoration: pageDecoration,
-            ),
-            PageViewModel(
-                title: "맛있으면",
-                body: '"바나나"',
-                image: _buildImage(
-                    'introduction_image/IntroductionImage_2.png', 510.w),
-                decoration: pageDecoration),
-            PageViewModel(
-                title: "맛있으면",
-                body: '"바나나"',
-                image: _buildImage(
-                    'introduction_image/IntroductionImage_3.png', 510.w),
-                decoration: pageDecoration),
-            PageViewModel(
-                title: "맛있으면",
-                body: '"바나나"',
-                image: _buildImage(
-                    'introduction_image/IntroductionImage_4.png', 510.w),
-                decoration: pageDecoration),
+          rawPages: [
+            _buildImageContainer('introduction_image/introduction_image_1.png'),
+            _buildImageContainer('introduction_image/introduction_image_2.png'),
+            _buildImageContainer('introduction_image/introduction_image_3.png'),
+            _buildStack(),
           ],
-          onDone: () => _onIntroEnd(context),
           skipFlex: 0,
           nextFlex: 0,
-          skip: const Text("Skip"),
-          next: const Icon(Icons.arrow_forward),
-          done: const Text("Done"),
+          showDoneButton: false,
+          showNextButton: false,
+          showSkipButton: false,
+          dotsDecorator: const DotsDecorator(
+            activeColor: Color.fromRGBO(255, 156, 30, 1),
+            size: Size(10.0, 10.0),
+            color: Color(0xFFBDBDBD),
+            activeSize: Size(20.0, 10.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildImageContainer(String assetName) {
+    return Container(
+      width: _width,
+      height: _height,
+      child: Image.asset('assets/images/$assetName'),
+    );
+  }
+
+  Widget _buildStack() {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: _width,
+          height: _height,
+          child: Image.asset(
+              'assets/images/introduction_image/introduction_image_4.png'),
+        ),
+        Positioned(
+          width: 190.w,
+          height: 42.h,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed('/home');
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              side: BorderSide(
+                width: 1.5.w,
+                color: Color.fromRGBO(255, 156, 30, 1),
+              ),
+            ),
+            child: Text(
+              '밀당 시작하기!',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+                fontSize: 16.sp,
+              ),
+            ),
+          ),
+          bottom: 80.h,
+          left: 110.75.w,
+        ),
+      ],
     );
   }
 }
