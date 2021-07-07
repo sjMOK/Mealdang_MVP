93bc8f613f9807b9b665cd9f7be7203f6054d67b
Hugnry-bird <rnjsgudtjr@naver.com>
2021-07-07 21:20:12 +0900

    add fix font size

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,12 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: Size(411.5, 683.4),
       builder: () => MaterialApp(
+        builder: (context, child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+            child: child,
+          );
+        },
         initialRoute: '/splash',
         routes: {
           '/splash': (context) => Splash(),
