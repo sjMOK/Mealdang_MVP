2388f71b21761cd2be04022f32279196139d9e27
Hugnry-bird <rnjsgudtjr@naver.com>
2021-07-16 02:02:53 +0900

    add image and url launcher

--- lib/page/mealdangHome.dart
+++ lib/page/mealdangHome.dart
@@ -1,4 +1,3 @@
-import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mealdang_mvp/database/db.dart';
